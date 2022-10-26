befd6ba409989cf0293af9a6be5ce6c7170c0e88
iiTzK <7assanelt0p@gmail.com>
2022-10-26 08:33:15 +0200

    كان في شغل كتير تاني بس ضاعو يلا مش مشكلة

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,47 +1,56 @@
-from cgitb import text
+from unittest import result
 import discord
 from discord.ext import commands
 import json
+import requests
+from difflib import get_close_matches
 
-class data():
+
+# TODO هعمل ان البيانات القراء وكل شئ تتحمل علي الجهاز ومتتمسحش في حال اختار الشخص يمسحه 
+# TODO + هعمل امر تحديث في لوحة CMD لو اتعملت او من اوامر البوت ذات نفسه
+# TODO لتسريع العمليه وتسهيل استهلاك الماموري واستخدام مساحه الجهاز عوضٌ عن الميمور
+# TODO وأخراج اقصي سرعه ممكنة للبوت
+
+class Data():
     def __init__(self):
         """ بيانات السيرفرات وأعداداتها `تفحص` , `وتعدل` هنا !"""
-        self.defaultData: dict = json.load(open("data.json", encoding='utf-8'))
+        self.defaultdata: dict = json.load(open("data.json", encoding='utf-8'))
 
     def check(self, guild: discord.Guild):
         """ JSON وظيفة الـ `فحص` اذا كان يوجد سيرفر جديد ام لاء من هنا وتحفظ في """
-        if str(guild.id) in self.defaultData.keys():
-            return self.defaultData[str(guild.id)]
+        if str(guild.id) in self.defaultdata.keys():
+            return self.defaultdata[str(guild.id)]
         else:
-            self.defaultData.update({ str(guild.id):{ "volume":1.0, "staychannel":{"status":False,"channelID":"id"}, "radio":"https://Qurango.net/radio/tarateel"} })
+            self.defaultdata.update({ str(guild.id):{ "volume":1, "staychannel":{"status":False,"channelID":"id"}, "radio":"https://Qurango.net/radio/tarateel"} })
             with open("data.json", "w") as data:
-                json.dump(self.defaultData, data, indent=4)
-            return self.defaultData[str(guild.id)]
+                json.dump(self.defaultdata, data, indent=4)
+            return self.defaultdata[str(guild.id)]
 
     def update(self, ctx: discord.ApplicationContext, volume: float = None, stayChannel:bool = None, channelId:str = None, radio:str = None):
         """ Json وظيفة الـ `تعديل` علي اي معلومة داخل """
         text = ""
         try:
-            guild = self.defaultData[str(ctx.guild.id)]
+            guild = self.defaultdata[str(ctx.guild.id)]
         except:
             self.check(guild)
-        if volume:
+        if volume and volume <= 150:
+            # لن يعمل في حال كانت القيمة اكتر من 150
             guild["volume"] = volume
             if guild["volume"] < volume:
                 text = "رفع صوت البوت"
             else:
                 text = "رفع صوت البوت"
         elif stayChannel:
-            guild["status"] = stayChannel
-            if guild["status"]:
+            guild["staychannel"]["status"] = stayChannel
+            if guild["staychannel"]["status"]:
                 text = "خاصية التثبيت تعمل"
             else:
                 text = "خاصية التثبيت متوقفه"
         elif channelId:
-            if guild["channelID"] == channelId:
+            if guild["staychannel"]["channelID"] == channelId:
                 text = "يبدو انك لمتضف شئ جديد"
             else:
-                guild["channelID"] = channelId
+                guild["staychannel"]["channelID"] = channelId
                 text = "تم اضافة غرفة جديد"
         elif radio:
             if guild["radio"] == radio:
@@ -53,12 +62,12 @@ class data():
             raise Exception("يبدو ان هناك خطا !")
         return text
 
-class quran():
+class Quran():
     def __init__(self, bot_:commands.Bot):
         self.bot = bot_
             
-    async def Radio(self, guild : discord.Guild):
-        data_ = data().check(guild)
+    async def radio_by_default(self, guild : discord.Guild): 
+        data_ = Data().check(guild)
         # ---------- Var
         staychannel = data_["staychannel"]
         radioURL = data_["radio"]
@@ -75,3 +84,108 @@ class quran():
             return
         guild.voice_client.play(source, after=lambda e: print(f"Player error: {e}") if e else None)
 
+    async def play_url(self, ctx: discord.ApplicationContext, reciter: str):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+            if ctx.voice_client.channel != ctx.author.voice.channel:
+                await ctx.voice_client.move_to(ctx.author.voice.channel)
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(reciter))
+        ctx.guild.voice_client.play(source, after=lambda e: print(f"Player error: {e}") if e else None)
+
+    async def play_reciter(self, ctx: discord.ApplicationContext, reciter:str, sura:int):
+        reciters = Reciters.get_reciters()
+        result = get_close_matches(reciter, reciters, cutoff=0.5)
+        reciterinfo = reciters[result[0]]
+        url = reciterinfo["server"]+"/"+'{:03}'.format(sura)+".mp3"
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+            if ctx.voice_client.channel != ctx.author.voice.channel:
+                await ctx.voice_client.move_to(ctx.author.voice.channel)
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url))
+        ctx.guild.voice_client.play(source, after=lambda e: print(f"Player error: {e}") if e else None)
+        await ctx.send(url)
+
+        
+
+    # ==========================
+    # TODO : مسودة
+    # ==========================
+    # def currentData(self, context: discord.ApplicationContext, args):
+    #     """يسحب البيانات الخاصة بالقارئ الذي يعمل في الخلفية `الصوت` و `رابط القارئ` و `ايدي الغرفة الصوتية`"""
+    #     changes = {"guild":str(context.guild.id)}
+    #     voice = context.voice_client
+    #     for value in args:
+    #         match value:
+    #             case 'volume':
+    #                 changes.update({'volume':voice.source.volume})
+    #             case 'channel':
+    #                 changes.update({'channel':str(voice.channel.id)})
+    #             case 'recter':
+    #                 changes.update({'recter':voice.source.original._process.args[2]})
+    #     return changes
+
+    
+    # class Dropdown(discord.ui.Select):
+    #     def __init__(self, ctx: discord.ApplicationContext, bot_: discord.Bot):
+    #         self.bot = bot_
+    #         self.defaultdata: dict = json.load(open("data.json", encoding='utf-8'))
+
+    #         options = [
+    #             discord.SelectOption(label="Recter", description="قناة القارئ التي تريدها", emoji="📻", value="recter"),
+    #             discord.SelectOption(label="Channel", description="الغرفة الصوتية الأفتراضيه", emoji="📞", value="channel"),
+    #             discord.SelectOption(label="Volume", description="صوت البوت", emoji="🔊", value="volume"),
+    #         ]
+
+    #         super().__init__(
+    #             placeholder="ماذا تريد حفظ من الاعدادت الحاليه",
+    #             min_values=1,
+    #             max_values=3,
+    #             options=options,
+    #         )
+    #         self.values_ = quran(self.bot).currentData(ctx, self.values)
+    #     async def callback(self, interaction: discord.Interaction):
+    #         guildData: dict = Data().check(interaction.guild)
+    #         for key , value in self.values_.items():
+    #             match key:
+    #                 case 'volume':
+    #                     guildData.update({'volume':value})
+    #                 case 'channel':
+    #                     guildData["staychannel"].update({'channelID':value})
+    #                 case 'recter':
+    #                     guildData.update({'radio':value})
+    #             with open('data.json', 'w', encoding='utf-8') as f:
+    #                 json.dump(guildData, f, indent=4)
+    #         await interaction.response.send_message(f"{key}: {value} !!")
+
+class Reciters:
+    def __init__(self):
+        pass
+        
+    def get_radio(self):
+        """Returns dict with `name` and `radio_url`"""
+        res = requests.get('https://api.mp3quran.net/radios/radio_arabic.json')
+        response: dict = json.loads(res.text)
+        return response["radios"]
+
+    def to_list(self, list: dict): return [ reciter for reciter in list.keys()]
+    
+    def get_reciters(): # هيتسمح عند التحديث ! الجديد 
+        res = requests.get('https://api.mp3quran.net/reciters/_arabic.json')
+        response = json.loads(res.text)
+        reciters = {}
+        for i in response["reciters"]:
+            reciters.update({
+            i["name"]: {"name":i["name"], "id":i['id'], "server":i['Server'], "rewaya":i['rewaya'], "count":i['count'], "letter":i['letter'], "suras":i['suras']}})
+        return reciters
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-from backend import quran
-
+from backend import Quran, Reciters
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,12 +11,22 @@ bot = commands.Bot(
     intents=intents,
 )
 
+quran = Quran(bot)
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user} (ID: {bot.user.id})")
     print("------")
     for guild in bot.guilds:
-        await quran(bot).Radio(guild)
+        await quran.radio_by_default(guild)
+
+@bot.command()
+async def play(ctx, recter: str):
+    await quran.play(ctx, recter)
+
+@bot.command()
+async def playR(ctx, recter:str, sura:int):
+    await quran.play_reciter(ctx, recter, sura)
 
 
-bot.run("عيب متبصش")
+bot.run("MTAxNDAzOTYxMjA3Njg2MzU2OA.G_hrUc.W7_HmPbrS9tkPdh8KyWVVw8yTbrmEXGqWjDgJI")
